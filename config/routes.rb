c928207f8848deb94b765618096508733ccfbb76
moeka kitamura <moeka.k76@gmail.com>
2022-11-20 19:14:52 +0900

    create conversation controller & model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+
+  resources :conversations do
+    resources :messages
+  end
 end
